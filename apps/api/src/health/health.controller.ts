95133a4af43d174f137fcd891f05d8b0bb724e9c
Jaypee Ignacio <jaypeeig@users.noreply.github.com>
2026-08-26 22:33:19 +0800

    Refactored health module

--- apps/api/src/health/health.controller.ts
+++ apps/api/src/health/health.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { HealthService } from "./health.service";
-import type { HealthStatus } from "./health.service.types";
+import type { HealthStatus } from "./health.types";
 
 @ApiTags("Health")
 @Controller("health")
--- apps/api/src/health/health.service.ts
+++ apps/api/src/health/health.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import type { HealthStatus } from "./health.service.types";
+import type { HealthStatus } from "./health.types";
 
 @Injectable()
 export class HealthService {
